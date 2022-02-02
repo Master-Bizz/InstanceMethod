24d7ea54b1955a923cfdf96ac6ab995fdbd955ad
Storm King <94218345+Master-Bizz@users.noreply.github.com>
2022-02-02 03:23:32 +0000

    appended

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+Instance Methods
+'''
+
 class Student:
    def __init__(self,scores = []):
        self.scores = scores
@@ -11,3 +15,21 @@ Tee = Student(scores = [1,21,3,1,25,6,17])
 print(bizz.avg())
 print(Tee.avg())
 
+'''
+Static Methods
+'''   # Doesn't require access to instance, absent of any 'self'.
+
+class Student:
+   def __init__(self,scores = []):
+       self.scores = scores
+
+   def avg(self):
+       return round (sum(self.scores) / len(self.scores))
+
+   @staticmethod
+   def notice():
+       return 'Be wise and revise!'
+
+Tj = Student(scores = [9,281,233,121,125,63,117])
+print(Student.notice())
+print(Tj.notice())
